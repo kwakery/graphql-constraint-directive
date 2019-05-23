c7a00d44e29fa42320a218760c1298c6d84889d3
Matt Tran <22088815+kwakery@users.noreply.github.com>
2019-05-23 13:17:07 -0700

    Update to work "better" in apollo-server2

--- scalars/number.js
+++ scalars/number.js
@@ -1,61 +1,60 @@
-const { GraphQLScalarType } = require('graphql')
-const ValidationError = require('../lib/error')
+const { GraphQLScalarType } = require('graphql');
+const { UserInputError } = require('apollo-server');
 
 module.exports = class ConstraintNumberType extends GraphQLScalarType {
-  constructor (fieldName, type, args) {
+  constructor(fieldName, type, args) {
     super({
       name: `ConstraintNumber`,
-      serialize (value) {
-        value = type.serialize(value)
+      serialize(value) {
+        value = type.serialize(value);
 
-        validate(fieldName, args, value)
+        validate(fieldName, args, value);
 
-        return value
+        return value;
       },
-      parseValue (value) {
-        value = type.serialize(value)
+      parseValue(value) {
+        value = type.serialize(value);
 
-        validate(fieldName, args, value)
+        validate(fieldName, args, value);
 
-        return type.parseValue(value)
+        return type.parseValue(value);
       },
-      parseLiteral (ast) {
-        const value = type.parseLiteral(ast)
+      parseLiteral(ast) {
+        const value = type.parseLiteral(ast);
 
-        validate(fieldName, args, value)
+        validate(fieldName, args, value);
 
-        return value
+        return value;
       }
-    })
+    });
   }
-}
+};
 
-function validate (fieldName, args, value) {
+function validate(fieldName, args, value) {
   if (args.min && value < args.min) {
-    throw new ValidationError(fieldName,
-      `Must be at least ${args.min}`,
-      [{ arg: 'min', value: args.min }])
+    throw new UserInputError(`${fieldName} must be at least ${args.min}`);
   }
+
   if (args.max && value > args.max) {
-    throw new ValidationError(fieldName,
-      `Must be no greater than ${args.max}`,
-      [{ arg: 'max', value: args.max }])
+    throw new UserInputError(
+      `${fieldName} must be no greater than ${args.max}`
+    );
   }
 
   if (args.exclusiveMin && value <= args.exclusiveMin) {
-    throw new ValidationError(fieldName,
-      `Must be greater than ${args.exclusiveMin}`,
-      [{ arg: 'exclusiveMin', value: args.exclusiveMin }])
+    throw new UserInputError(
+      `${fieldName} must be greater than ${args.exclusiveMin}`
+    );
   }
   if (args.exclusiveMax && value >= args.exclusiveMax) {
-    throw new ValidationError(fieldName,
-      `Must be no greater than ${args.exclusiveMax}`,
-      [{ arg: 'exclusiveMax', value: args.exclusiveMax }])
+    throw new UserInputError(
+      `${fieldName} must be no greater than ${args.exclusiveMax}`
+    );
   }
 
   if (args.multipleOf && value % args.multipleOf !== 0) {
-    throw new ValidationError(fieldName,
-      `Must be a multiple of ${args.multipleOf}`,
-      [{ arg: 'multipleOf', value: args.multipleOf }])
+    throw new UserInputError(
+      `${fieldName} must be a multiple of ${args.multipleOf}`
+    );
   }
 }
